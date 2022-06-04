d32e723d7375b7b3a5232ebcc6fd3048ed8d934d
pasi kanto <68000803+Pkanto@users.noreply.github.com>
2022-06-04 16:09:08 +0300

    sivuston valmistelu

--- ablog/blogi/urls.py
+++ ablog/blogi/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import HomeView, Artikkeli
+
 
 urlpatterns = [
-    path('', views.home, name = "home"),
+    path('', HomeView.as_view(), name= 'home'),
+    path('artikkeli/<int:pk>', Artikkeli.as_view(), name = 'artikkeli'),
 ]
