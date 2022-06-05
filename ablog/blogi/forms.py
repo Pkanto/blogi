23154d19458db09c9f0be8e298dc179b1f3fe736
pasi kanto <68000803+Pkanto@users.noreply.github.com>
2022-06-05 10:43:11 +0300

    Poisto nappula ja ulkoasu muokkausta

--- ablog/blogi/forms.py
+++ ablog/blogi/forms.py
@@ -4,9 +4,10 @@ from .models import Post
 class mallikaavio(forms.ModelForm):
   class Meta:
     model = Post
-    fields = ('title', 'body')
+    fields = ('title','author', 'body')
 
     widgets = {
       'title':forms.TextInput(attrs= {'class': 'form-control','placeholder':'Otsikko'}),
+      'author':forms.Select(attrs= {'class': 'form-control','placeholder':'Otsikko'}),
       'body':forms.Textarea(attrs= {'class': 'form-control', 'placeholder': 'Blogi teksti'})
     }
--- ablog/blogi/urls.py
+++ ablog/blogi/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import HomeView, Artikkeli, kirjoitaBlogi, Paivita
+from .views import HomeView, Artikkeli, kirjoitaBlogi, Paivita, Poista
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name= 'home'),
     path('artikkeli/<int:pk>', Artikkeli.as_view(), name = 'artikkeli'),
     path('blogin/', kirjoitaBlogi.as_view(), name= 'bloggin'),
-    path('editoi/<int:pk>', Paivita.as_view(), name = 'paivita')
+    path('artikkeli/paivita/<int:pk>', Paivita.as_view(), name = 'paivita'),
+    path('artikkeli/<int:pk>/poista', Poista.as_view(), name = 'poista'),
 ]
