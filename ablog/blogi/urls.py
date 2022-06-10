dc584950bac01f4cabea2425fcf0b07d1d75968e
pasi kanto <68000803+Pkanto@users.noreply.github.com>
2022-06-10 17:13:32 +0300

    kategoria lista teko

--- ablog/blogi/urls.py
+++ ablog/blogi/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import HomeView, Artikkeli, kirjoitaBlogi, Paivita, Poista
+from .views import HomeView, Artikkeli, kirjoitaBlogi, Paivita, Poista, TeeKategoria, Kategoriat
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name= 'home'),
     path('artikkeli/<int:pk>', Artikkeli.as_view(), name = 'artikkeli'),
     path('blogin/', kirjoitaBlogi.as_view(), name= 'bloggin'),
+    path('lisaa_kategoria/', TeeKategoria.as_view(), name= 'kategoria'),
     path('artikkeli/paivita/<int:pk>', Paivita.as_view(), name = 'paivita'),
     path('artikkeli/<int:pk>/poista', Poista.as_view(), name = 'poista'),
+    path('kategoria/<str:kat>/', Kategoriat, name = 'kategori'),
 ]
--- ablog/blogi/views.py
+++ ablog/blogi/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Kategoria
 from .forms import mallikaavio
 from django.urls import reverse_lazy
 
@@ -12,7 +12,19 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
   model = Post
   template_name = 'home.html'
+  kats = Kategoria.objects.all()
   ordering = ['-julkaisu']
+
+  def get_context_data(self, *args, **kwargs):
+    kat_lista = Kategoria.objects.all()
+    context = super(HomeView, self).get_context_data(*args, **kwargs)
+    context["kat_lista"] = kat_lista
+    return context
+
+def Kategoriat(request, kat):
+  kategoria_posts = Post.objects.filter(kategoria = kat.capitalize().replace('-',' '))
+  return render(request, 'kategoriat.html',{'kat':kat.replace('-',' ').capitalize(), 'kategoria_posts':kategoria_posts})
+
 
 class Artikkeli(DetailView):
   model = Post
@@ -23,7 +35,13 @@ class kirjoitaBlogi(CreateView):
   form_class = mallikaavio
   template_name = 'blogging.html'
   success_url = reverse_lazy('home')
-  #fields = ('title', 'body')
+  
+
+class TeeKategoria(CreateView):
+  model = Kategoria
+  fields ='__all__'
+  template_name = 'kategoria.html'
+  success_url = reverse_lazy('home')
 
 class Paivita(UpdateView):
   model = Post
@@ -34,3 +52,4 @@ class Poista(DeleteView):
   model = Post
   template_name = 'poista.html'
   success_url = reverse_lazy('home')
+
