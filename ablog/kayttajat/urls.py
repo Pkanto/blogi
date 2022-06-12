bb5db7d7051a11734cbadfcf97697666bebd688c
pasi kanto <68000803+Pkanto@users.noreply.github.com>
2022-06-12 09:11:27 +0300

    Salasana vaihto lomake ja sivu

--- ablog/kayttajat/urls.py
+++ ablog/kayttajat/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TeeKayttaja
-
-
+from .views import TeeKayttaja,MuokkaaKayttajaa, Salasanavaihto
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('rekistroi/', TeeKayttaja.as_view(), name='rekisteroi'),
+    path('muokkaa/', MuokkaaKayttajaa.as_view(), name='muokkaa'),
+    path('password/',Salasanavaihto.as_view(template_name='registration/vaihda_salasana.html')),
+    path('salasana_onnistui', views.salasana_onnistui, name='salasana_vaihto'),
 ]
--- ablog/kayttajat/views.py
+++ ablog/kayttajat/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import Kirjaudu
+from .forms import Kirjaudu, MuokkaaProfiialomake,SalasanaVaihtolomake
+
+class Salasanavaihto(PasswordChangeView):
+    form_class = SalasanaVaihtolomake
+    success_url = reverse_lazy('salasana_vaihto')
 
 class TeeKayttaja(generic.CreateView):
     form_class = Kirjaudu
     template_name = 'registration/rekisteroi.html'
     success_url = reverse_lazy('login')
+
+class MuokkaaKayttajaa(generic.UpdateView):
+    form_class = MuokkaaProfiialomake
+    template_name = 'registration/muokkaa_profiilia.html'
+    success_url = reverse_lazy('home')
+    def get_object(self):
+        return self.request.user
+
+def salasana_onnistui(request):
+    return render(request, 'registration/salasana_onnistui.html', {})
