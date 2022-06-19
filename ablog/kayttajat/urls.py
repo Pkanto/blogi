5599c6f69f2fb4d9acd02b5a8614590198861132
pasi kanto <68000803+Pkanto@users.noreply.github.com>
2022-06-19 15:15:41 +0300

    käyttäjä profiilin editointi sivu

--- ablog/kayttajat/urls.py
+++ ablog/kayttajat/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TeeKayttaja,MuokkaaKayttajaa, Salasanavaihto,NaytaProfiili
+from .views import TeeKayttaja,MuokkaaKayttajaa, Salasanavaihto,NaytaProfiili, EditoiProfiilia
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('password/',Salasanavaihto.as_view(template_name='registration/vaihda_salasana.html')),
     path('salasana_onnistui', views.salasana_onnistui, name='salasana_vaihto'),
     path('<int:pk>/kayttajaprofiili',NaytaProfiili.as_view(), name = 'nayta_profiili'),
+    path('<int:pk>/editoiprofiilia',EditoiProfiilia.as_view(), name = 'editoi_profiili'),
 ]
--- ablog/kayttajat/views.py
+++ ablog/kayttajat/views.py
@@ -6,11 +6,27 @@ from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import Kirjaudu, MuokkaaProfiialomake,SalasanaVaihtolomake
 from blogi.models import Profiili
+from django.db import models
+from django.contrib.auth.models import User
+
+class EditoiProfiilia(generic.UpdateView):
+    model =Profiili
+    template_name = "registration/editoi_profiilia.html"
+    fields = ['bio', 'profiili_kuva', 'some_fb','some_insta', 'some_linke']
+
+    user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
+    bio = models.TextField()
+    profiili_kuva = models.ImageField(null = True, blank = True,upload_to ='images/profiili/') #proffilin mallin editointi salliin oneToone liitoksen tietokantaan
+    some_fb = models.CharField(max_length = 255, null = True, blank = True)
+    some_insta = models.CharField(max_length = 255, null = True, blank = True)
+    some_linke = models.CharField(max_length = 255, null = True, blank = True)
+    success_url = reverse_lazy('home')
 
 
 class NaytaProfiili(DetailView):
     model = Profiili
     template_name = 'registration/kayttaja_profiili.html'
+    
 
     def get_context_data(self, *args, **kwargs): #hakee kirjautuneen käyttäjän ID ja antaa sen käyttöön
         kayttaja = get_object_or_404(Profiili, id = self.kwargs['pk'])
